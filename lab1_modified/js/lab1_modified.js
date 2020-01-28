b87ee1080e126949b62fdbf7d099fef2de6f333a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2020-01-28 15:30:31 +0000

    remove useless var, fix content type

--- lab1_modified/js/lab1_modified.js
+++ lab1_modified/js/lab1_modified.js
@@ -1,20 +1,19 @@
 var http = require('http'); //import http lib
 var fs = require('fs'); //import filesystem lib
 const path = require('path'); //import path lib
-const staticDir = "../html";
 
 function serveStatic(res, fileName, contentType, responseCode) {
     if (!responseCode) responseCode = 200;
 
     var filePath = path.join(__dirname, "..", "html", fileName);
-    console.log(filePath);
+
     fs.readFile(filePath, function(err, data) {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain'});
             res.end("500 - Internal Error");
         } else {
             res.writeHead(responseCode, { 'Content-Type': contentType });
-            res.end(data, 'utf-8');           
+            res.end(data);           
         }
     })
 }
@@ -25,15 +24,15 @@ http.createServer(function(req,res){
     let reqPath = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase(); //regex to remove vars from path
     switch(reqPath) {
         case '':
-            serveStatic(res, "home.html", "html/text", 200);
+            serveStatic(res, "home.html", "text/html", 200);
             break;
         case '/products':
-                serveStatic(res, "products.html", "html/text", 200);
+            serveStatic(res, "products.html", "text/html", 200);
             break;
         default:
-                serveStatic(res, "404.html", "html/text", 404);
+            serveStatic(res, "404.html", "text/html", 404);
             break;
         }
     }).listen(8000);
     
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+console.log('Server started on localhost:8000; press Ctrl-C to terminate....');
